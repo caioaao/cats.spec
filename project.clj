37cfa301ea77ea1521434995b11837fc42a7bc2f
caio <caioaao@gmail.com>
2017-05-09 01:30:52 -0300

    License stub

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cats.spec "0.1.0-SNAPSHOT"
   :description "specs for funcool cats monads"
   :url "http://github.com/caioaao/cats.spec"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :license {:name "BSD (2 Clause)"
+            :url  "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [org.clojure/spec.alpha "0.1.94"]
                  [org.clojure/test.check "0.9.0"]
