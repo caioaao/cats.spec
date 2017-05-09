a07cd1d8bec394f5b6dfd1df2379ae802cfab79b
caio <caioaao@gmail.com>
2017-05-09 00:45:44 -0300

    Exception spec + :nail_care:

---
+++ src/cats/spec/monad/exception.clj
@@ -0,0 +1,68 @@
+(ns cats.spec.monad.exception
+  (:require [clojure.spec.alpha :as s]
+            [cats.monad.exception :as m.exc]
+            [clojure.spec.gen.alpha :as gen]))
+
+(defn exception-impl
+  [form-success pred-success form-failure pred-failure]
+  (let [spec-success (delay (s/specize* pred-success form-success))
+        spec-failure (delay (s/specize* pred-failure form-failure))]
+    (reify
+      s/Specize
+      (specize* [s] s)
+      (specize* [s _] s)
+
+      s/Spec
+      (conform* [_ x]
+        (let [conformed-v (delay (s/conform* (if (m.exc/success? x) @spec-success @spec-failure) @x))]
+          (cond
+            (or (not (m.exc/exception? x))
+                (= @conformed-v ::s/invalid)
+                (and (m.exc/failure? x) (not (instance? Exception @conformed-v))))
+            ::s/invalid
+
+            (m.exc/success? x)
+            (m.exc/success @conformed-v)
+
+            (m.exc/failure? x)
+            (m.exc/failure @conformed-v))))
+
+      (unform* [_ x]
+        (let [unformed-v (delay (s/unform* (if (m.exc/success? x) @spec-success @spec-failure) @x))]
+          (cond
+            (or (not (m.exc/exception? x))
+                (= @unformed-v ::s/invalid)
+                (and (m.exc/failure? x) (not (instance? Exception @unformed-v))))
+            ::s/invalid
+
+            (m.exc/success? x)
+            (m.exc/success @unformed-v)
+
+            (m.exc/failure? x)
+            (m.exc/failure @unformed-v))))
+
+      (explain* [_ path via in x]
+        (cond
+          (not (m.exc/exception? x)) {:path path :pred `m.exc/exception? :val x :via via :in in}
+          (m.exc/success? x) (s/explain* @spec-success
+                                         (conj path :either/right)
+                                         via in @x)
+          (m.exc/failure? x) (s/explain* @spec-failure
+                                         (conj path :either/left)
+                                         via in @x)))
+
+      (gen* [this overrides path rmap]
+        (if-let [gfn (:gfn this)]
+          (gfn)
+          (gen/frequency
+           [[1 (gen/fmap m.exc/success (s/gen* @spec-success overrides path rmap))]
+            [1 (gen/fmap m.exc/failure (s/gen* @spec-failure overrides path rmap))]])))
+
+      (with-gen* [this gfn] (assoc this :gfn gfn))
+
+      (describe* [_]
+        `(either ~form-success ~form-failure)))))
+
+(defmacro exception
+  [pred-success pred-failure]
+   `(exception-impl '~pred-success ~pred-success '~pred-failure ~pred-failure))
--- test/cats/spec/monad/either_test.clj
+++ test/cats/spec/monad/either_test.clj
@@ -5,48 +5,48 @@
             [clojure.spec.gen.alpha :as gen]
             [clojure.test :as t]))
 
-(s/def :player/name string?)
-(s/def :player/gender #{:male :female})
-(s/def :player/age (s/and int? pos? #(< % 120)))
-(s/def :player/player (s/keys :req [:player/name :player/gender :player/age]))
+(s/def :either.player/name string?)
+(s/def :either.player/gender #{:male :female})
+(s/def :either.player/age (s/and int? pos? #(< % 120)))
+(s/def :either.player/player (s/keys :req [:either.player/name :either.player/gender :either.player/age]))
 
-(s/def :move/delta int?)
-(s/def :move/player :player/player)
-(s/def :move/move (s/keys :req [:move/delta :move/player]))
+(s/def :either.move/delta int?)
+(s/def :either.move/player :either.player/player)
+(s/def :either.move/move (s/keys :req [:either.move/delta :either.move/player]))
 
-(s/def :move/error string?)
+(s/def :either.move/error string?)
 
-(s/def :move/move-or-error (s.either/either :move/move :move/error))
+(s/def :either/move-or-error (s.either/either :either.move/move :either.move/error))
 
 
 (t/deftest either-spec-test
   (t/testing "Works with clojure.spec api"
-    (doseq [v (gen/sample (s/gen :move/move-or-error))]
-      (t/is (s/valid? :move/move-or-error v))
+    (doseq [v (gen/sample (s/gen :either/move-or-error))]
+      (t/is (s/valid? :either/move-or-error v))
       (t/is (m.either/either? v))
       (t/is (= v
-               (s/conform :move/move-or-error v)
-               (s/unform :move/move-or-error v)
-               (s/unform :move/move-or-error (s/conform :move/move-or-error v))))
-      (t/is (or (and (m.either/right? v) (s/valid? :move/move @v))
-                (and (m.either/left? v) (s/valid? :move/error @v)))))
+               (s/conform :either/move-or-error v)
+               (s/unform :either/move-or-error v)
+               (s/unform :either/move-or-error (s/conform :either/move-or-error v))))
+      (t/is (or (and (m.either/right? v) (s/valid? :either.move/move @v))
+                (and (m.either/left? v) (s/valid? :either.move/error @v)))))
 
-    (t/is (= (s/describe :move/move-or-error) '(either :move/move :move/error))))
+    (t/is (= (s/describe :either/move-or-error) '(either :either.move/move :either.move/error))))
 
   (t/testing "Explain works for `either?` pred"
-    (t/is (-> (s/explain-data :move/move-or-error 1)
+    (t/is (-> (s/explain-data :either/move-or-error 1)
               ::s/problems
-              (= {:path [], :pred `m.either/either?, :val 1, :via [:move/move-or-error], :in []}))))
+              (= {:path [], :pred `m.either/either?, :val 1, :via [:either/move-or-error], :in []}))))
 
   (t/testing "Explain works for any side of the branch"
-    (let [right-v (gen/generate (s/gen :move/move))
-          left-v (gen/generate (s/gen :move/error))]
-      (t/is (-> (s/explain-data :move/move-or-error (m.either/right left-v))
+    (let [right-v (gen/generate (s/gen :either.move/move))
+          left-v (gen/generate (s/gen :either.move/error))]
+      (t/is (-> (s/explain-data :either/move-or-error (m.either/right left-v))
                 ::s/problems
                 first
-                (= {:path [:either/right], :pred 'map?, :val left-v, :via [:move/move-or-error], :in []})))
+                (= {:path [:either/right], :pred 'map?, :val left-v, :via [:either/move-or-error], :in []})))
 
-      (t/is (-> (s/explain-data :move/move-or-error (m.either/left right-v))
+      (t/is (-> (s/explain-data :either/move-or-error (m.either/left right-v))
                 ::s/problems
                 first
-                (= {:path [:either/left], :pred 'string?, :val right-v, :via [:move/move-or-error], :in []}))))))
+                (= {:path [:either/left], :pred 'string?, :val right-v, :via [:either/move-or-error], :in []}))))))
